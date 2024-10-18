58523780e82dfb426a14202b428a1777f57851ee
thaidinh <thai562003@gmail.com>
2024-10-18 12:33:05 +0700

    Update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,7 +14,7 @@ const Home = () => (
             <title>Yến Sào LeGia'Nest </title>
             <meta
                 name="description"
-                content="Yến Sào LeGia Nest chuyên phân phối tổ yến tươi, yến sào, yến chưng nguyên chất 100%, cam kết CHẤT LƯỢNG – KHÔNG PHA TRỘN. Với mong muốn mang đến nguồn sản phẩm NÂNG CAO SỨC KHỎE cho người dùng, Legia'Nest luôn đặt chất lượng sản phẩm lên hàng đầu, đặc biệt Yến sào Legia'Nest đảm bảo giữ nguyên vị thuần túy 100% từ tổ Yến tự nhiên."
+                content="Yến Sào LeGia Nest chuyên phân phối tổ yến tươi, yến sào, yến chưng nguyên chất 100%, cam kết CHẤT LƯỢNG – KHÔNG PHA TRỘN. "
             />
             <meta name="keywords" content="yến tươi, yến sào, yến chưng, set quà tặng, LegiaNest" />
             <meta name="author" content="Yến Sào LeGia'Nest" />

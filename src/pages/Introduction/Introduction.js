10014e50c108e2a8a517a52ae729d296a630758f
thaidinh <thai562003@gmail.com>
2024-10-18 12:51:02 +0700

    Update

--- src/pages/Introduction/Introduction.js
+++ src/pages/Introduction/Introduction.js
@@ -57,12 +57,11 @@ const Introduction = () => {
                 <meta
                     name="description"
                     content={
-                        pageContent.description ||
-                        "Yến Sào LeGia Nest chuyên phân phối tổ yến tươi, yến sào, yến chưng nguyên chất 100%, cam kết CHẤT LƯỢNG – KHÔNG PHA TRỘN. Với mong muốn mang đến nguồn sản phẩm NÂNG CAO SỨC KHỎE cho người dùng, Legia'Nest luôn đặt chất lượng sản phẩm lên hàng đầu, đặc biệt Yến sào Legia'Nest đảm bảo giữ nguyên vị thuần túy 100% từ tổ Yến tự nhiên."
+                        'Yến Sào LeGia Nest chuyên phân phối tổ yến tươi, yến sào, yến chưng nguyên chất 100%, cam kết CHẤT LƯỢNG – KHÔNG PHA TRỘN để đảm bảo giữ nguyên vị thuần túy 100% từ tổ Yến tự nhiên.'
                     }
                 />
-                <meta name="keywords" content="giới thiệu, LegiaNest, thông tin hợp tác xã" />
-                <meta name="author" content="Yến Sào LeGia Nest " />
+                <meta name="keywords" content={`${pageContent.name}, LegiaNest`} />
+                <meta name="author" content="Yến Sào LeGia'Nest" />
             </Helmet>
             <div className={cx('inner')}>
                 <Title subText={pageContent.name} />

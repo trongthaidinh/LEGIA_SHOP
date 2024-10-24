2bde1a46b0b55b0598a6dfed61e3e1d120bdc3a0
thaidinh <thai562003@gmail.com>
2024-10-25 01:01:33 +0700

    Update Certificate

--- src/pages/Home/Certification/Certification.js
+++ src/pages/Home/Certification/Certification.js
@@ -6,12 +6,15 @@ import Title from 'components/TitleSub';
 const cx = classNames.bind(styles);
 
 const mockCertificates = [
-    { id: 1, url: 'https://lagianest.com/wp-content/uploads/2022/11/quang-cao-yen-1.2.jpg', title: 'Chứng nhận 2' },
+    {
+        id: 1,
+        url: 'https://res.cloudinary.com/drioug4df/image/upload/v1729792595/514a5cb7-b6ba-48f6-905e-dccc1513d4d0_rnv1k8.jpg',
+        title: 'Chứng nhận 2',
+    },
     {
         id: 2,
         url: 'https://res.cloudinary.com/drioug4df/image/upload/v1728013729/ec36f79c-f114-4004-959d-93dc38f7b82d.jpg',
     },
-    { id: 3, url: 'https://lagianest.com/wp-content/uploads/2022/11/quang-cao-yen-1.3.jpg', title: 'Chứng nhận 3' },
 ];
 
 function Certification() {
